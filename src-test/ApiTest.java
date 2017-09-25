78ab94048c7dd2cb5c9d13dd9f4127ddf6491673
Joosep Hint <joosep.hint@ekool.eu>
2017-09-25 12:37:10 +0300

    First actual commit

---
+++ src-test/ApiTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ApiTest {
+
+    @Test
+    public void testCorrectJSON() {
+
+    }
+
+    @Test
+    public void testCorrectMaxTemperature(){
+
+    }
+
+    @Test
+    public void testCorrectMinTemperature() {
+
+    }
+
+    @Test
+    public void testCorrectMomentTemperature() {
+
+    }
+
+    @Test
+    public void testCorrectTemperaturesAmountForThreeDays() {
+
+    }
+
+    @Test
+    public void testCorrectCoordinates() {
+
+    }
+}
